a9b769a6134926dff781d3215570cb5b6be0543e
Vladimir Pavkin <vpavkin@gmail.com>
2020-03-24 13:37:33 +0300

    Simplify value class format

--- src/main/g8/src/main/scala/$package$/api/protocol.scala
+++ src/main/g8/src/main/scala/$package$/api/protocol.scala
@@ -10,7 +10,7 @@ final case class UpdateItemRequest(name: String, price: BigDecimal)
 final case class PartialUpdateItemRequest(name: Option[String], price: Option[BigDecimal])
 
 trait JsonSupport extends PlayJsonSupport {
-  implicit val itemIdFormat                   = implicitly[Format[Long]].inmap[ItemId](ItemId, _.value)
+  implicit val itemIdFormat                   = Json.valueFormat[ItemId]
   implicit val itemFormat                     = Json.format[Item]
   implicit val createItemRequestFormat        = Json.format[CreateItemRequest]
   implicit val updateItemRequestFormat        = Json.format[UpdateItemRequest]
