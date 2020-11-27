55d333cff64f52b1510e114b712242cf671623ee
Patryk Kirszenstein <thebestilspl@gmail.com>
2020-11-27 08:20:14 +0100

    Create IT Postgres tests with runner

--- src/main/g8/src/it/scala/$package$/application/ItItemRepositorySpec.scala
+++ src/main/g8/src/it/scala/$package$/application/ItItemRepositorySpec.scala
@@ -1,40 +1,44 @@
 package $package$.application
 
-import zio._
-import $package$.application.ApplicationService
-import $package$.domain.{ItemRepository, _}
-import $package$.interop.akka.ZioRouteTest
+import $package$.ITSpec.ITSpec
+import $package$.domain.{ ItemRepository, _ }
+import $package$.infrastructure.flyway.FlywayProvider
+import zio.ZIO
 import zio.test.Assertion._
-import zio.test.{ Spec, TestFailure, TestSuccess, suite, testM, _ }
+import zio.test.{ suite, testM, _ }
+import zio.test.TestAspect._
 
-object ItItemRepositorySpec extends ZioRouteTest with PosgresContainer {
+object ItItemRepositorySpec extends ITSpec(Some("items")) {
+
+  val migrateDbSchema =
+    FlywayProvider.flyway
+      .flatMap(_.migrate)
+      .toManaged_
 
   def flippingFailure(value: Any): Exception =
-      new Exception(
-      "Flipping failed! The referred effect was successful with `" + value + "` result of `"+ value.getClass + "` type!"
+    new Exception(
+      "Flipping failed! The referred effect was successful with `" + value + "` result of `" + value.getClass + "` type!"
     )
 
-  private val env = containerRepository
-
   private def allItems: ZIO[ItemRepository, Throwable, List[Item]] = ApplicationService.getItems.mapError(_.asThrowable)
-  allItems
-  private val specs: Spec[ItemRepository, TestFailure[Object], TestSuccess] =
+
+  migrateDbSchema.useNow
+  val spec: ITSpec =
     suite("Item Repository")(
       //  def addItem
       testM("Add correct item ") {
         val name: String      = "name"
         val price: BigDecimal = 100.0
         for {
-          _: ItemId     <- ApplicationService.addItem(name, price)
+          _: ItemId       <- ApplicationService.addItem(name, price)
           contentsCheck <- assertM(allItems)(equalTo(List(Item(ItemId(1), "name", 100.0))))
-
         } yield contentsCheck
       },
       testM("Should add different item and fail in assertion ") {
         val name: String      = "name1"
         val price: BigDecimal = 100.0
         for {
-          _: ItemId     <- ApplicationService.addItem(name, price)
+          _: ItemId       <- ApplicationService.addItem(name, price)
           contentsCheck <- assertM(allItems)(equalTo(List(Item(ItemId(1), "name", 100.0))))
 
         } yield !contentsCheck
@@ -42,19 +46,20 @@ object ItItemRepositorySpec extends ZioRouteTest with PosgresContainer {
       testM("Should not allow to add wrong data to db") {
         val name: String      = ""
         val price: BigDecimal = null
+        migrateDbSchema.useNow
         for {
           error <- ApplicationService.addItem(name, price).flip.orDieWith(flippingFailure)
         } yield assert(error.toString)(equalTo("RepositoryError(java.lang.NullPointerException)"))
       },
-      $if(add_caliban_endpoint.truthy)$
-        //             def getItemByName
+      //             def getItemByName
       testM("Get correct item by name ") {
         val name: String      = "name"
         val price: BigDecimal = 100.0
         for {
+
           _: ItemId <- ApplicationService.addItem(name, price)
           _: ItemId <- ApplicationService.addItem(name, price + 5)
-          item      <- ApplicationService.getItemByName(name)
+          item    <- ApplicationService.getItemByName(name)
 
         } yield assert(item)(equalTo(List(Item(ItemId(1), name, 100.00), Item(ItemId(2), name, 105.00))))
       },
@@ -74,20 +79,19 @@ object ItItemRepositorySpec extends ZioRouteTest with PosgresContainer {
           _: ItemId <- ApplicationService.addItem(name, price + 15)
           _: ItemId <- ApplicationService.addItem(name, price + 45)
           _: ItemId <- ApplicationService.addItem(name, price + 75)
-          item      <- ApplicationService.getItemsCheaperThan(120.0)
+          item    <- ApplicationService.getItemsCheaperThan(120.0)
 
         } yield assert(item)(
           equalTo(List(Item(ItemId(1), name, 100.00), Item(ItemId(2), name, 105.00), Item(ItemId(3), name, 115.00)))
         )
       },
-      $endif$
-        //  def getItem
+      //  def getItem
       testM("Get correct item ") {
         val name: String      = "name"
         val price: BigDecimal = 100.0
         for {
           _: ItemId <- ApplicationService.addItem(name, price)
-          item      <- ApplicationService.getItem(ItemId(1))
+          item    <- ApplicationService.getItem(ItemId(1))
 
         } yield assert(item)(equalTo(Some(Item(ItemId(1), name, 100.00))))
       },
@@ -103,7 +107,7 @@ object ItItemRepositorySpec extends ZioRouteTest with PosgresContainer {
         for {
           _: ItemId <- ApplicationService.addItem(name, price)
           _: ItemId <- ApplicationService.addItem(name, price + 5)
-          _         <- ApplicationService.getItems
+          _       <- ApplicationService.getItems
           contentsCheck <- assertM(allItems)(
             equalTo(List(Item(ItemId(1), name, 100.00), Item(ItemId(2), name, 105.00)))
           )
@@ -116,7 +120,7 @@ object ItItemRepositorySpec extends ZioRouteTest with PosgresContainer {
         for {
           _: ItemId <- ApplicationService.addItem(name, price)
           _: ItemId <- ApplicationService.addItem(name, price + 5)
-          _         <- ApplicationService.getItems
+          _       <- ApplicationService.getItems
           contentsCheck <- assertM(allItems)(
             equalTo(List(Item(ItemId(1), name, 100.00), Item(ItemId(2), name, 105.00)))
           )
@@ -129,7 +133,7 @@ object ItItemRepositorySpec extends ZioRouteTest with PosgresContainer {
         for {
           _: ItemId <- ApplicationService.addItem(name, price)
           _: ItemId <- ApplicationService.addItem(name, price)
-          _         <- ApplicationService.deleteItem(ItemId(1))
+          _       <- ApplicationService.deleteItem(ItemId(1))
 
           contentsCheck <- assertM(allItems)(equalTo(List(Item(ItemId(2), "name", 100.0))))
 
@@ -145,7 +149,7 @@ object ItItemRepositorySpec extends ZioRouteTest with PosgresContainer {
         val name: String      = "name"
         val price: BigDecimal = 100.0
         for {
-          _: ItemId     <- ApplicationService.addItem(name, price)
+          _: ItemId       <- ApplicationService.addItem(name, price)
           _             <- ApplicationService.updateItem(ItemId(1), "dummy", 123.2)
           contentsCheck <- assertM(allItems)(equalTo(List(Item(ItemId(1), "dummy", 123.2))))
 
@@ -161,7 +165,7 @@ object ItItemRepositorySpec extends ZioRouteTest with PosgresContainer {
         val name: String      = "name"
         val price: BigDecimal = 100.0
         for {
-          _: ItemId     <- ApplicationService.addItem(name, price)
+          _: ItemId       <- ApplicationService.addItem(name, price)
           _             <- ApplicationService.partialUpdateItem(ItemId(1), None, Some(777.7))
           contentsCheck <- assertM(allItems)(equalTo(List(Item(ItemId(1), "name", 777.7))))
 
@@ -172,26 +176,6 @@ object ItItemRepositorySpec extends ZioRouteTest with PosgresContainer {
           update <- ApplicationService.partialUpdateItem(ItemId(1), None, Some(777.7))
         } yield assert(update)(equalTo(None))
       }
-      //      //  def deletedEvents
-      //      testM("delete event run without anything ") {
-      //        val name: String      = "name"
-      //        val price: BigDecimal = 100.0
-      //        for {
-      //          _: ItemId <- ApplicationService.addItem(name, price)
-      //          things    <- ApplicationService.deletedEvents.runCollect
-      //        } yield assert(things.toList)(equalTo(List(ItemId(1))))
-      //      },
-      //  def deletedEvents
-      //      testM("delete event ") {         This test is freezing propably whole infrastructure of events is made wrong.
-      //        val name: String      = "name"
-      //        val price: BigDecimal = 100.0
-      //        for {
-      //          _: ItemId <- ApplicationService.addItem(name, price)
-      //          _         <- ApplicationService.deleteItem(ItemId(1))
-      //          things    <- ApplicationService.deletedEvents.runCollect
-      //        } yield assert(things.toList)(equalTo(List(ItemId(1))))
-      //      }
-    )
+    ) @@ before(FlywayProvider.flyway.flatMap(_.migrate).orDie)
 
-  def spec: ZSpec[_root_.zio.test.environment.TestEnvironment, Any] = specs.provideLayer(env.mapError(TestFailure.die))
 }
