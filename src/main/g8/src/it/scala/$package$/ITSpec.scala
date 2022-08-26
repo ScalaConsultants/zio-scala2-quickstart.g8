804fda013b84ce3b155156467c078f2b15524280
Eder Ruiz <edrzmr@gmail.com>
2022-08-25 23:56:30 -0300

    update: Fix it/ITSpec.scala

--- src/main/g8/src/it/scala/$package$/ITSpec.scala
+++ src/main/g8/src/it/scala/$package$/ITSpec.scala
@@ -1,82 +1,58 @@
 package $package$
 
+import com.typesafe.config.{ Config, ConfigFactory }
+import slick.jdbc.PostgresProfile
+import slick.interop.zio.DatabaseProvider
+
+import zio._
+import zio.test.ZIOSpecDefault
+import zio.logging.backend.SLF4J
+
 import $package$.domain.ItemRepository
 import $package$.infrastructure.{ Postgres, SlickItemRepository }
 import $package$.infrastructure.Postgres.SchemaAwarePostgresContainer
 import $package$.infrastructure.flyway.FlywayProvider
-import com.typesafe.config.{ Config, ConfigFactory }
-import slick.interop.zio.DatabaseProvider
-import slick.jdbc.JdbcProfile
-import zio.blocking.Blocking
-import zio.duration.durationInt
-import zio.logging._
-import zio.logging.slf4j.Slf4jLogger
-import zio.test._
-import zio.test.environment.TestEnvironment
-import zio.{ Has, Layer, ULayer, ZLayer }
+
 import scala.jdk.CollectionConverters.MapHasAsJava
 
-object ITSpec {
-  type Postgres = Has[SchemaAwarePostgresContainer]
-  type ITEnv    = TestEnvironment with Has[FlywayProvider] with Logging with Has[ItemRepository]
+abstract class ITSpec(schema: Option[String]) extends ZIOSpecDefault {
 
-  abstract class ITSpec(schema: Option[String] = None) extends RunnableSpec[ITEnv, Any] {
-    type ITSpec = ZSpec[ITEnv, Any]
+  val itLayers: ZLayer[Scope, Throwable, FlywayProvider with ItemRepository] = {
 
-    override def aspects: List[TestAspect[Nothing, ITEnv, Nothing, Any]] =
-      List(TestAspect.timeout(60.seconds))
+    val logging: ULayer[Unit] = ZLayer.make[Unit](
+      Runtime.removeDefaultLoggers,
+      SLF4J.slf4j
+    )
 
-    override def runner: TestRunner[ITEnv, Any] =
-      TestRunner(TestExecutor.default(itLayer))
+    val postgres: URLayer[Scope, SchemaAwarePostgresContainer] = Postgres.postgres(schema)
 
-    val blockingLayer: Layer[Nothing, Blocking]       = Blocking.live
-    val postgresLayer: ZLayer[Any, Nothing, Postgres] = blockingLayer >>> Postgres.postgres(schema)
-    val dbLayer: ZLayer[
-      Any with Postgres with Blocking,
-      Nothing,
-      TestEnvironment with Has[FlywayProvider] with Logging with Has[ItemRepository]
-    ] = {
+    val config: URLayer[SchemaAwarePostgresContainer, Config] = ZLayer {
+      for {
+        container <- ZIO.service[SchemaAwarePostgresContainer]
+      } yield ConfigFactory.parseMap(
+        Map(
+          "url"            -> container.jdbcUrl,
+          "user"           -> container.username,
+          "password"       -> container.password,
+          "driver"         -> "org.postgresql.Driver",
+          "connectionPool" -> "HikariCP",
+          "numThreads"     -> 1,
+          "queueSize"      -> 100
+        ).asJava
+      )
+    }
 
-      val config: ZLayer[Postgres, Nothing, Has[Config]] = ZLayer
-        .fromService[SchemaAwarePostgresContainer, Config] { container =>
-          val config = ConfigFactory.parseMap(
-            Map(
-              "url"            -> container.jdbcUrl,
-              "user"           -> container.username,
-              "password"       -> container.password,
-              "driver"         -> "org.postgresql.Driver",
-              "connectionPool" -> "HikariCP",
-              "numThreads"     -> 1,
-              "queueSize"      -> 100
-            ).asJava
-          )
-          config
-        }
+    val jdbcProfile: ULayer[PostgresProfile] = ZLayer.succeed(PostgresProfile)
 
-      val dbProvider: ZLayer[Postgres with Any, Throwable, Has[DatabaseProvider]] =
-        config ++ ZLayer.succeed[JdbcProfile](slick.jdbc.PostgresProfile) >>> DatabaseProvider.live
-
-      val flyWayProvider = config >>> FlywayProvider.live
-
-      val postgresLayer = Postgres.postgres(schema)
-      val blockingLayer = Blocking.live
-
-      val containerDatabaseProvider: ZLayer[Blocking, Throwable, Has[DatabaseProvider]] =
-        blockingLayer >>> postgresLayer >>> dbProvider
-
-      val containerRepository: ZLayer[Blocking, Throwable, Has[ItemRepository]] =
-        (Logging.ignore ++ containerDatabaseProvider) >>> SlickItemRepository.live
-
-      val logging = Slf4jLogger.make { (context, message) =>
-        val logFormat     = "[correlation-id = %s] %s"
-        val correlationId = LogAnnotation.CorrelationId.render(
-          context.get(LogAnnotation.CorrelationId)
-        )
-        logFormat.format(correlationId, message)
-      }
-      zio.test.environment.testEnvironment ++ flyWayProvider ++ logging ++ containerRepository
-    }.orDie
-
-    val itLayer: ULayer[ITEnv] = postgresLayer ++ blockingLayer >>> dbLayer
+    ZLayer.makeSome[Scope, FlywayProvider with ItemRepository](
+      logging,
+      jdbcProfile,
+      config,
+      postgres,
+      DatabaseProvider.live,
+      SlickItemRepository.live,
+      FlywayProvider.live
+    )
   }
+
 }
